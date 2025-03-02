2f86cf4240d1682cb0dc6867054e18aaf66a563c
jacoblamoureux <jacoblamoureux@unomaha.edu>
2025-03-02 19:09:15 +0000

    word count is done.

--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,23 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
-  for line in textFile:
-    print(line)
-  
+  textFile = open("fish.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
 
+  for line in textFile:
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+      for l in w:
+        if l.isalpha():
+          letterCount = letterCount + 1
+    #print(line)
+  
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letters:", letterCount)
 if __name__ == '__main__':
   main()
